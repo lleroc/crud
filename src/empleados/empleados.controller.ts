57f8b9433940cb2f6bc6de145a44cd018246b734
Lleroc <llleroc@yahoo.es>
2024-03-08 20:11:13 -0500

    controlador

--- src/empleados/empleados.controller.ts
+++ src/empleados/empleados.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { EmpleadosService } from './empleados.service';
+import { IEmpleado } from './empleado.interface';
 
 @Controller('empleados')
-export class EmpleadosController {}
+export class EmpleadosController {
+  constructor(private empleadosServicio: EmpleadosService) {}
+
+  @Get()
+  todos() {
+    return this.empleadosServicio.todos();
+  }
+
+  @Get(':id')
+  uno(@Param('id') id: string) {
+    return this.empleadosServicio.uno(id);
+  }
+  @Post()
+  insertar(@Body() empleado: IEmpleado) {
+    return this.empleadosServicio.insertar(empleado);
+  }
+  @Put(':id')
+  actualizar(@Param('id') id: string, @Body() empleado: IEmpleado) {
+    return this.empleadosServicio.actualizar(id, empleado);
+  }
+  @Delete(':id')
+  eliminar(@Param('id') id: string) {
+    return this.empleadosServicio.eliminar(id);
+  }
+}
